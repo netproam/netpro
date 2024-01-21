dd6f295e4d0911c648ee145c1d2d238bd9c0039c
Ouzar Merouane <ouzar.merouane@gmail.com>
2024-01-20 21:30:39 -0500

    ajout du formulaire de contact

--- src/components/section/ContactSection.tsx
+++ src/components/section/ContactSection.tsx
@@ -11,7 +11,7 @@ const ContactSection = () => {
         <div className="h-fit lg:w-4/5 w-full  gap-4 flex flex-col text-center lg:text-start">
     <p className="font-semibold text-sm ">{`Contacter nous`.toLocaleUpperCase()}</p>
     <h3 className="text-3xl  w-3/5 self-center
-    leading-relaxed lg:w-full lg:text-5xl tracking-tighter font-semibold">Besoin d'un <span className="text-[#88CBCE]">Netoyage?</span>Prennez contacte avec nous!</h3>
+    leading-relaxed lg:w-full lg:text-5xl tracking-tighter font-semibold">{`Besoin d'un`} <span className="text-[#88CBCE]">Netoyage?</span>Prennez contacte avec nous!</h3>
     </div>
     <div className="p-4">
         <MapComponent/>
