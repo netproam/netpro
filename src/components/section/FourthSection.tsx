417b0d9d6201eba22502c26d141661c94d5e71fe
Ouzar Merouane <ouzar.merouane@gmail.com>
2024-01-21 00:01:21 -0500

    une premiere version presentable je dirais surtout sur pc sur mobile elle devrait être pas mal mais je vais justement la checker là

--- src/components/section/FourthSection.tsx
+++ src/components/section/FourthSection.tsx
@@ -29,14 +29,16 @@ else {
   return (
     <section 
     ref={ref}
-    className="h-[600px] py-20 items-center 
+    className="h-fit py-20 items-center 
     lg:px-10 justify-center gap-20  lg:justify-between bg-[#F0F9F9] w-full flex flex-col lg:flex-row">
 
-<div className="flex flex-col justify-center items-center lg:items-start  w-full gap-4">
+<div className="flex flex-col justify-center items-center lg:items-start  w-full gap-10">
+<div className="flex flex-col gap-2">
 <h4 className="text-sm font-semibold">Ce que nous apportons</h4>
 <h3 className="font-semibold tracking-tighter text-4xl  lg:text-6xl text-center lg:text-start">Des solutions de nettoyages pour tous</h3>
-<p>{`Nous sommes en constante croissance`}</p>
-<div className="flex gap-20 items-center  ">
+<p className="pt-4">{`Nous sommes en constante croissance`}</p>
+</div>
+<div className="flex gap-10 py-8   lg:gap-20 items-center  lg:py-0 lg:pt0 ">
 <Button className="bg-[#A42503] rounded-full py-6">Nous contacter</Button>
 <div className="flex items-center gap-2">
     <div className="w-fit p-2 rounded-full border-2 border-slate-300">
@@ -47,30 +49,38 @@ else {
 </div>
 </div>
 
-<div className="flex flex-row lg:flex-col justify-center  lg:justify-between w-full gap-20 lg:gap-10 items-center  ">
-    <div className="flex 
-     
-    flex-col lg:flex-row w-full gap-4 lg:gap-10 items-end justify-end  ">
-     <div className="flex flex-col lg:flex-row w-fit lg:w-full items-start">
-        <motion.p className="font-semibold lg:w-full   w-fit text-5xl tracking-tighter"><motion.span>{rounded}</motion.span><motion.span className="relative -inset-y-6">+</motion.span></motion.p>
-        <div className="flex flex-col  gap-2  pt-8 w-full ">
-        <p className="font-bold text-lg">Clients</p>
-        <p>Nous ont fais confiances</p>
-        </div>
-        </div>
+<div className="w-full flex flex-row justify-center items-center lg:flex-col gap-20 lg:gap-10">
+<div className="flex flex-col lg:flex-row gap-6 lg:gap-8 w-full  justify-end">
+    <div className="w-full flex flex-col justify-end items-end lg:items-start ">
+    <motion.p className="font-semibold text-5xl hidden lg:flex tracking-tighter w-fit "><motion.span>{rounded}</motion.span>
+    
+    <motion.span className="-inset-y-4 relative">+</motion.span>
+    </motion.p>
     </div>
- 
-    <div className="flex 
-     
-     flex-col lg:flex-row w-full gap-4 lg:gap-10 items-end justify-end  ">
-      <div className="flex flex-col lg:flex-row w-fit lg:w-full items-start ">
-         <motion.p className="font-semibold lg:w-full   w-fit text-5xl tracking-tighter"><motion.span>{countEmployer}</motion.span><motion.span className="relative -inset-y-6">+</motion.span></motion.p>
-         <div className="flex flex-col  gap-2  pt-8 w-full ">
-         <p className="font-bold text-lg">Employes</p>
-         <p>En tout temps disponibles pour convenir à vos besoins</p>
-         </div>
-         </div>
-     </div>
+    <div className="w-fit  self-end lg:w-full gap-4 lg:gap-0 flex flex-col  lg:self-start relative ">
+    <motion.p className="font-semibold text-5xl  lg:hidden tracking-tighter w-fit "><motion.span>{rounded}</motion.span>
+    <motion.span className="-inset-y-4 relative">+</motion.span>
+    </motion.p>
+
+        <h3 className="font-semibold text-lg text-start relative  ">Clients</h3>
+        <p className="whitespace-nowrap">Ils nous ont fais confiance</p>
+    </div>
+</div>
+
+{/**deuxieme section */}
+<div className="flex flex-col lg:flex-row gap-6 lg:gap-8 w-full  justify-end">
+    <div className="w-full flex flex-col justify-end items-end lg:items-start ">
+    <motion.p className="font-semibold text-5xl hidden lg:flex tracking-tighter w-fit "><motion.span>{roundedEmpl}</motion.span></motion.p>
+    </div>
+    <div className="w-fit  self-end lg:w-full gap-4 lg:gap-0 flex flex-col  lg:self-start relative ">
+    <motion.p className="font-semibold text-5xl  lg:hidden tracking-tighter w-fit "><motion.span>{roundedEmpl}</motion.span>
+    
+    </motion.p>
+
+        <h3 className="font-semibold text-lg text-start relative  ">{`Employés`}</h3>
+        <p className="">{`Compétant, discret et efficace à votre services`}</p>
+    </div>
+</div>
 </div>
     </section>
   )
