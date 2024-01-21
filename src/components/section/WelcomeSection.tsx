6f1c8b24b1233f019925d9d16e6b330f4b227ae7
Ouzar Merouane <ouzar.merouane@gmail.com>
2024-01-21 17:11:00 -0500

    debout d'ajout de la data

--- src/components/section/WelcomeSection.tsx
+++ src/components/section/WelcomeSection.tsx
@@ -1,8 +1,8 @@
 "use client"
-import { useEffect, useState } from "react"
+import { useEffect, useState ,useRef} from "react"
 import Navbar from "../Navigation/Navbar"
 import { Button } from "../ui/button"
-import {motion ,AnimatePresence} from "framer-motion"
+import {motion ,AnimatePresence,useInView} from "framer-motion"
 import { v4 } from "uuid"
 import {useMediaQuery} from "react-responsive"
 const WelcomeSection = () => {
@@ -17,8 +17,11 @@ isMobile:`https://images.pexels.com/photos/209271/pexels-photo-209271.jpeg?auto=
 ,{isDesktop:`https://images.pexels.com/photos/4099467/pexels-photo-4099467.jpeg?auto=compress&cs=ti
 nysrgb&w=1260&h=750&dpr=2`,isMobile:"https://images.pexels.com/photos/3287162/pexels-photo-3287162.jpeg?auto=compress&cs=tinysrgb&w=800"}]
 
-
+const refWelcome=useRef(null)
+const isInView=useInView(refWelcome,{amount:0.5})
     useEffect(()=>{
+        if(isInView&&isInterValId===undefined)
+        {
         let clearInterValId=setInterval(()=>{
             setIsChangingImage(e=>{
                 
@@ -35,10 +38,15 @@ nysrgb&w=1260&h=750&dpr=2`,isMobile:"https://images.pexels.com/photos/3287162/pe
         
     },5000)
     setIsIntervalId(clearInterValId)
+}
+else {
+
+}
     return ()=>{
-        clearInterval(clearInterValId)
+        clearInterval(isInterValId)
+
     }
-},[])
+},[isInView,isInterValId])
 
 const allText=[{title:`Entretien et nettoyage de bureaux et locaux professionnels`,
 id:v4(),
@@ -59,14 +67,23 @@ return (
     <div 
     key={e.id}
     className="flex flex-col justify-center">
-          <h4 className="text-center hidden lg:flex self-center  text-slate-300 font-semibold tracking-tighter">{e.sousDescription}</h4>
-    <h3 className="text-slate-200 text-2xl text-center lg:text-4xl   leading-relaxed  lg:leading-tight 
-    font-semibold tracking-tighter">{e.title}</h3>
-    <div className="flex gap-8 justify-center text-lg pt-10">
+          <motion.h4 
+              animate={{opacity:[0,1],transition:{duration:1.5,delay:0.75}}}
+
+          className="text-center hidden lg:flex self-center  text-slate-300
+           font-semibold tracking-tighter">{e.sousDescription}</motion.h4>
+    <motion.h3
+    animate={{opacity:[0,1],transition:{duration:3,delay:1.25}}}
+    className="text-slate-200 text-2xl text-center lg:text-4xl   leading-relaxed  lg:leading-tight 
+    font-semibold tracking-tighter">{e.title}</motion.h3>
+    <motion.div 
+                  animate={{opacity:[0,1],transition:{duration:1.75,delay:2}}}
+
+    className="flex gap-8 justify-center text-lg pt-10">
         <Button className="bg-blue-900 text-slate-100 p-4 rounded-full text-lg">Voir plus</Button>
         <Button className="bg-[#88CBCE] p-4 rounded-full text-lg ">Notre service</Button>
 
-    </div>
+    </motion.div>
     </div>
 )
 })
@@ -93,34 +110,41 @@ function relunchInterval(e:number)
 }
 
   return (
-  <section className="w-screen  h-screen relative flex flex-col  items-center justify-center">
+  <section 
+  ref={refWelcome}
+  className="w-screen  h-screen relative flex flex-col  items-center justify-center">
    
     <Navbar/>
+    <div className="flex absolute w-screen h-screen bg-black">
+
 <AnimatePresence>
 {isChangingImage===0?<motion.img 
+animate={{scale:[1,1.1]}} transition={{duration:1.5}}
  
 key={"firstImg"}
 
-className="object-center  absolute w-full h-full  brightness-50 " 
+className="object-center  relative w-full h-full  brightness-50 " 
 src={isTabletOrMobile? allImageData[isChangingImage].isMobile :allImageData[isChangingImage].isDesktop}/>
 : isChangingImage===1? 
 <motion.img 
-
+animate={{scale:[1,1.1]}} transition={{duration:1.5}}
 key={"secondImg"}
 
-className="object-center  absolute w-full h-full  brightness-50 " 
+className="object-center  relative w-full h-full  brightness-50 " 
 src={isTabletOrMobile? allImageData[isChangingImage].isMobile :allImageData[isChangingImage].isDesktop}/>
 :
 isChangingImage===2&&<motion.img 
 
+animate={{scale:[1,1.1]}} transition={{duration:1.5}}
 
 key={"thirdImg"}
 
 
-className="object-center  absolute w-full h-full  brightness-50 " 
+className="object-center  relative w-full h-full  brightness-50 " 
 src={isTabletOrMobile? allImageData[isChangingImage].isMobile :allImageData[isChangingImage].isDesktop}/>
 }
 </AnimatePresence>
+</div>
   <section className="flex flex-col relative z-20 gap-0 text-center ">
   {allCompnent[isChangingImage]}
   </section>
