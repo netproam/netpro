008958d2b38e0f55f040f1af08ff617fbf41db56
Ouzar Merouane <ouzar.merouane@gmail.com>
2024-01-22 14:35:02 -0500

    j'ai corrigé le touch probleme sur mobile

--- src/components/section/FourthSection.tsx
+++ src/components/section/FourthSection.tsx
@@ -32,11 +32,11 @@ else {
     className="h-fit py-20 items-center 
     lg:px-10 justify-center gap-20  lg:justify-between bg-[#F0F9F9] w-full flex flex-col lg:flex-row">
 
-<div className="flex flex-col justify-center items-center lg:items-start  w-full gap-10">
-<div className="flex flex-col gap-2">
+<div className="flex flex-col lg:px-8 justify-center items-center lg:items-start  w-full gap-10">
+<div className="flex flex-col gap-2 ">
 <h4 className="text-sm font-semibold text-center lg:text-start">Ce que nous apportons</h4>
-<h3 className="font-semibold tracking-tighter text-4xl  lg:text-6xl text-center lg:text-start">Des solutions de nettoyages pour tous</h3>
-<p className="pt-8 text-center lg:text-start ">{`Nous sommes en constante croissance`}</p>
+<h3 className="font-semibold tracking-tighter text-4xl  lg:text-5xl leading-relaxed text-center lg:text-start">Des solutions adaptés à vos besoins</h3>
+<p className="pt-8 text-center lg:text-start ">{`Nous sommes à votre écoute pour vous fournir des solutions personalisées et adaptées pour libérer votre temps. `}</p>
 </div>
 
 {/**Version mobile */}
@@ -69,7 +69,7 @@ else {
 
     </section>
 <div className="flex gap-10 pb-8   lg:gap-20 items-center  lg:py-0 lg:pt0 ">
-<Button className="bg-[#A42503] rounded-full py-6">Nous contacter</Button>
+<Button className="bg-[#A42503] text-white text-md rounded-full  font-semibold py-8 px-6 shadow-lg">Nous contacter</Button>
 <div className="flex items-center gap-2">
     <div className="w-fit p-2 rounded-full border-2 border-slate-300">
         <TbPlayerPlayFilled size={20}/>
@@ -82,16 +82,16 @@ else {
 <div className="w-full px-10 flex flex-row justify-center items-center lg:flex-col gap-0 lg:gap-10 hidden lg:flex">
 <div className="flex flex-col lg:flex-row gap-6 lg:gap-8 w-full  justify-end">
     <div className="w-full flex flex-col justify-end items-end lg:items-start ">
-    <motion.p className="font-semibold relative   text-5xl hidden lg:flex tracking-tighter w-fit "><motion.span className="relative">{rounded}
+    <motion.p className="font-semibold relative   text-8xl hidden lg:flex tracking-tighter w-fit "><motion.span className="relative">{rounded}
     
     
     </motion.span>
-    <motion.span className="-inset-y-4 absolute inset-x-20">+</motion.span>
+    <motion.span className="-inset-y-8 text-6xl absolute inset-x-40">+</motion.span>
 
     </motion.p>
     </div>
     <div className="w-fit  self-end lg:w-full gap-4 lg:gap-0 flex flex-col  lg:self-start relative ">
-    <motion.p className="font-semibold text-5xl  lg:hidden tracking-tighter w-fit relative "><motion.span>{rounded}</motion.span>
+    <motion.p className="font-semibold text-8xl  lg:hidden tracking-tighter w-fit relative "><motion.span>{rounded}</motion.span>
     <motion.span className="-inset-y-4 absolute">+</motion.span>
     </motion.p>
 
@@ -103,7 +103,7 @@ else {
 {/**deuxieme section */}
 <div className="flex flex-col lg:flex-row gap-6 lg:gap-8 w-full  justify-end">
     <div className="w-full flex flex-col justify-end items-end lg:items-start ">
-    <motion.p className="font-semibold text-5xl hidden lg:flex tracking-tighter w-fit "><motion.span>{roundedEmpl}</motion.span></motion.p>
+    <motion.p className="font-semibold text-8xl hidden lg:flex tracking-tighter w-fit "><motion.span>{roundedEmpl}</motion.span></motion.p>
     </div>
     <div className="w-fit  self-end lg:w-full gap-4 lg:gap-0 flex flex-col  lg:self-start relative ">
     <motion.p className="font-semibold text-5xl  lg:hidden tracking-tighter w-fit "><motion.span>{roundedEmpl}</motion.span>
