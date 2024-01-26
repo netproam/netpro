8b25397763ceabb5efbe9f7180f42be825c3b715
Ouzar Merouane <ouzar.merouane@gmail.com>
2024-01-26 03:30:14 -0500

    version plus paufiner

--- src/components/section/FooterSection.tsx
+++ src/components/section/FooterSection.tsx
@@ -1,9 +1,9 @@
 "use client"
 const FooterSection = () => {
   return (
-  <footer className="w-screen bg-black h-[400px] text-slate-100  p-4
+  <footer className="w-screen bg-black h-[400px] text-slate-100  p-4 lg:items-center
   lg:flex lg:justify-between grid grid-cols-2 place-items-center lg:p-20 ">
-<div className="flex flex-col  text-slate-400 gap-10">
+<div className="flex flex-col  text-slate-400 gap-10 w-full">
 <h4 className="font-semibold text-slate-50">Bureau</h4>
 <div className="flex flex-col justify-between h-full text-sm lg:text-lg">
 
