ed28dcfaad2f4809289f2c39cc61760a69fc4e7d
Ouzar Merouane <ouzar.merouane@gmail.com>
2024-01-26 03:27:33 -0500

    version plus paufiner

--- src/components/section/FooterSection.tsx
+++ src/components/section/FooterSection.tsx
@@ -1,19 +1,19 @@
 "use client"
 const FooterSection = () => {
   return (
-  <footer className="w-screen bg-black h-[400px] text-slate-100 
+  <footer className="w-screen bg-black h-[400px] text-slate-100  p-4
   lg:flex lg:justify-between grid grid-cols-2 place-items-center lg:p-20 ">
 <div className="flex flex-col  text-slate-400 gap-10">
 <h4 className="font-semibold text-slate-50">Bureau</h4>
-<div className="flex flex-col justify-between h-full">
+<div className="flex flex-col justify-between h-full text-sm lg:text-lg">
 
-<p>785 15h Street, Office 478
+<p className="w-4/5"> 785 15h Street, Office 478
 Berlin, De 81566</p>
 <p>info@example.com</p>
 <p className="font-bold">+1 800 123 45 67</p>
 </div>
 </div>
-<div className="flex flex-col text-slate-400 gap-10">
+<div className="flex flex-col text-slate-400 gap-10 w-full">
 <p className="text-slate-50 font-semibold"> Liens</p>
 <div className="flex flex-col justify-between h-full">
 <p>Facebook</p>
@@ -36,7 +36,7 @@ Berlin, De 81566</p>
 <p>Facebook</p>
 </div>
 </div>
-<div className="flex flex-col text-slate-400 gap-10 items-end w-full">
+<div className="flex flex-col text-slate-400 gap-10 items-start w-full">
 <p className="text-slate-50 font-semibold"> Liens</p>
 <div className="flex flex-col justify-between h-full">
 <p>Facebook</p>
