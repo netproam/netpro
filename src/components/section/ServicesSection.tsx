5bd42992ea63fed06e93fbef205505f296f02ddd
Ouzar Merouane <ouzar.merouane@gmail.com>
2024-01-29 14:43:51 -0500

    version a montrer au client

--- src/components/section/ServicesSection.tsx
+++ src/components/section/ServicesSection.tsx
@@ -17,14 +17,26 @@ const ServicesSection = () => {
     et la propreté professionnelle avec nos services de nettoyage commercial.`}]
 
     const allServices=[{
-       titre:"Nettoyage ",svg:<YourSvg style={{width:"100%",heigth:"100%"}}  />
+       titre:`Nettoyage des parties communes d’immeubles`,svg:<YourSvg style={{width:"100%",heigth:"100%"}}  />
     },{
-        titre:"Entretien",description:"Désinfection",svg:<DesinfectSVG style={{width:"100%",heigth:"100%"}}/>
+        titre:`Nettoyage des copropriétés`,description:"Désinfection",svg:<DesinfectSVG style={{width:"100%",heigth:"100%"}}/>
     },{
-        titre:"Copropriété",description:"the best thing over",svg:<GlovesSVG style={{width:"100%",heigth:"100%"}}/>
+        titre:`⁠⁠Nettoyage de bureaux`,description:"the best thing over",svg:<GlovesSVG style={{width:"100%",heigth:"100%"}}/>
     },{
-        titre:"Débarras",description:"the best thing over",svg:<MovinSvg style={{width:"100%",heigth:"100%"}}/>
-    }]
+        titre:`Nettoyage de commerce`,description:"the best thing over",svg:<MovinSvg style={{width:"100%",heigth:"100%"}}/>
+    },{
+        titre:`Nettoyage de parking`,description:"the best thing over",svg:<MovinSvg style={{width:"100%",heigth:"100%"}}/>
+    },
+    {
+        titre:`⁠⁠Décapage des sols`,description:"the best thing over",svg:<MovinSvg style={{width:"100%",heigth:"100%"}}/>
+    },{
+        titre:`Nettoyage avant ou après déménagement `,description:"the best thing over",svg:<MovinSvg style={{width:"100%",heigth:"100%"}}/>
+    },{
+        titre:`Débarras`,description:"the best thing over",svg:<MovinSvg style={{width:"100%",heigth:"100%"}}/>
+    }
+
+
+]
     const allDataDisplay=allServices.map((e)=>{
        return <div 
        className="flex  flex-col w-full justify-start"
@@ -41,7 +53,7 @@ const ServicesSection = () => {
     })
   return (
    <section className="w-screen h-fit bg-[#F0F9F9] py-20
-    flex flex-col justify-center items-center  gap-10  lg:px-10">
+    flex flex-col justify-center items-center  gap-20  lg:px-10">
 
 <div className="flex flex-col lg:flex-row justify-between gap-10 lg:gap-0">
     <div className="flex text-sm flex-col w-full gap-4">
@@ -55,9 +67,16 @@ const ServicesSection = () => {
 <div>
 
 </div>
-<div className=" grid grid-cols-2     w-screen lg:justify-center
-gap-10  lg:gap-0 divided-none lg:flex lg:divide-x-[1px] divide-slate-300 ">
-{allDataDisplay}
+<h2 className=" font-semibold text-5xl">{`Nettoyages`}</h2>
+
+<div className=" grid grid-cols-3 lg:gap-10    w-screen lg:justify-center
+gap-2  divided-none lg:grid-cols-3 lg:divide-x-[1px] divide-slate-300 ">
+{allDataDisplay.slice(0,6)}
+</div>
+<h2 className=" relative  font-semibold text-5xl">{`Débarras`}</h2>
+<div className=" grid grid-cols-2       w-screen lg:justify-center
+gap-2  lg:gap-0 divided-none lg:grid-cols-2 place-items-center lg:divide-x-[1px] divide-slate-300 ">
+{allDataDisplay.slice(4,6)}
 </div>
    </section>
   )
--- src/components/services/ServiceComponent.tsx
+++ src/components/services/ServiceComponent.tsx
@@ -19,7 +19,7 @@ const ServiceComponent = ({title,children}:ServiceComponentProps) => {
     const isTabletOrMobile = useMediaQuery({ query: '(max-width: 804px)' })
 console.log(isTabletOrMobile)
   return (
-   <motion.div  className="flex  
+   <motion.div  className="flex   text-center
     flex-col gap-4 cursor-pointer  items-center 
     justify-center lg:items-center lg:justify-start w-full"
   onHoverStart={async()=>{
@@ -33,12 +33,10 @@ setIsAnime(`#containerTest`,{x:[null,0]},{duration:0.5})
     if(isTouched===false)
     {
    setIsAnime(`#childrenHolder`,{color:"black"},{duration:1})
-setIsAnime(`#containerTest`,{x:[null,0]},{duration:0.5})
 setIsTouched(true)
 }
 else {
     setIsAnime(`#childrenHolder`,{color:"#88CBCE"},{duration:1})
-    setIsAnime(`#containerTest`,{x:[0,-100]},{duration:0.5})
     setIsTouched(false)
 }
 
@@ -46,7 +44,6 @@ else {
    ref={isScope}
  onHoverEnd={()=>{
     setIsAnime(`#childrenHolder`,{color:"#88CBCE"},{duration:1})
-    setIsAnime(`#containerTest`,{x:[null,-100]},{duration:0.5})
 
 
 }}
@@ -56,22 +53,12 @@ else {
      w-20 lg:w-fit  flex items-center justify-center `} id={"childrenHolder"}>
 {children}
 </div>
-<h2 className="font-semibold  whitespace-nowrap  self-center  ">{title}</h2>
+<h2 className="font-semibold text-sm   self-center  w-2/3 ">{title}</h2>
 
 <motion.div
 layout
 className="flex justify-start  overflow-hidden ">
-    <motion.div
-    initial={{x:-100}}
-    id="containerTest"
 
-    className="flex gap-4 w-full relative z-50 ">
-    <motion.p  className="font-semibold text-xs lg:text-sm  whitespace-nowrap"
-  
-    id={`explores`}>{`Découvrir plus`}</motion.p>
-        <FaArrowRight id="arrowRight" size={15} className="relative inset-x-3"/>
-
-    </motion.div>
 </motion.div>
    </motion.div>
   )
