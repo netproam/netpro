6ec09e7685a95201da41d15e59ab1dc5eb5a2d1f
Ouzar Merouane <ouzar.merouane@gmail.com>
2024-01-29 00:18:24 -0500

    version a montrer au client

--- src/components/section/ServicesSection.tsx
+++ src/components/section/ServicesSection.tsx
@@ -17,13 +17,13 @@ const ServicesSection = () => {
     et la propreté professionnelle avec nos services de nettoyage commercial.`}]
 
     const allServices=[{
-       titre:"Nettoyages ",svg:<YourSvg style={{width:"100%",heigth:"100%"}}  />
+       titre:"Nettoyage ",svg:<YourSvg style={{width:"100%",heigth:"100%"}}  />
     },{
         titre:"Entretien",description:"Désinfection",svg:<DesinfectSVG style={{width:"100%",heigth:"100%"}}/>
     },{
         titre:"Copropriété",description:"the best thing over",svg:<GlovesSVG style={{width:"100%",heigth:"100%"}}/>
     },{
-        titre:"Debarrassage",description:"the best thing over",svg:<MovinSvg style={{width:"100%",heigth:"100%"}}/>
+        titre:"Débarras",description:"the best thing over",svg:<MovinSvg style={{width:"100%",heigth:"100%"}}/>
     }]
     const allDataDisplay=allServices.map((e)=>{
        return <div 
