fd4731334973e2c6e635bcc0a86ab4ac993e52af
Ouzar Merouane <ouzar.merouane@gmail.com>
2024-01-29 20:15:11 -0500

    version a montrer au client

--- src/app/services/layout.tsx
+++ src/app/services/layout.tsx
@@ -5,7 +5,7 @@ import Navbar from "@/components/Navigation/Navbar";
 export default function LayoutServices({
   children}: {children: React.ReactNode}) 
   {
-  return( <section className={"flex  flex-col relative bg-[#F0F9F9] "}>
+  return( <section className={"flex  flex-col relative bg-[#F0F9F9] overflow-hidden "}>
      
         <div className="pt-20 bg-[#F0F9F9] z-10 relative">
         {children}
