bd645b7761fbb5ae2af83eb8f099c65788ef9dac
Ouzar Merouane <ouzar.merouane@gmail.com>
2024-02-09 03:29:33 -0500

    inspiration de nikita netoyage pour la page temoignage

--- src/components/Navigation/DialogEstimation.tsx
+++ src/components/Navigation/DialogEstimation.tsx
@@ -28,24 +28,22 @@ export function DialogEstimation({styleBtn}:DialogProps) {
         <Button className={` text-center w-fit   relative flex items-center justify-center text-slate-200 px-8 py-0 rounded-lg
            font-semibold text-sm ${styleBtn}`} >Devis gratuit <span className=" absolute right-2"><MdKeyboardArrowRight size={20}/></span></Button>
       </DialogTrigger>
-      <DialogContent className="w-full h-full p-0 overflow-scroll bg-white ">
-        <DialogHeader>
-          <DialogTitle className="text-center text-lg py-4">Outils de simulation</DialogTitle>
-        
-          <div className="flex  w-full justify-center p-0 py-4 ">
-                <div className="w-fit rounded-2xl  bg-slate-200 text-sm  text-slate-800 gap-10  ">
-                <button className={`${isChanging? "bg-white rounded-2xl font-semibold text-slate-800 " : "bg-transparent"} p-2`} onClick={()=>setIsChanging(true)}>Estimation gratuite </button>
+      <DialogContent className="w-full h-full flex flex-col  p-0 overflow-scroll bg-white ">
+      
+        <div className="flex  w-full justify-center p-0 pt-16 pb-8 ">
+                <div className="w-fit rounded-2xl   text-xl  text-slate-800 gap-10  ">
+                <button className={`${isChanging? " bg-clip-text text-transparent bg-gradient-to-r from-[#04A9F4] to-[#17CCCA]  rounded-2xl font-semibold text-slate-white " : "bg-transparent"} p-2`} onClick={()=>setIsChanging(true)}>Estimation gratuite </button>
 
                 <button 
-                className={`${isChanging===false? "bg-white rounded-2xl font-semibold text-slate-800 " : "bg-transparent"} p-2`} 
+                className={`${isChanging===false? "bg-clip-text text-transparent bg-gradient-to-r from-[#04A9F4] to-[#17CCCA]  rounded-2xl font-semibold text-slate-white  " : "bg-transparent"} p-2`} 
                 onClick={()=>setIsChanging(false)}>Contactez nous </button>
                 </div>
                 </div>
-        </DialogHeader>
-       {isChanging? <div className="flex  flex-col   w-full">
+
+       {isChanging? <div className="flex  flex-col h-full   w-full">
          
        <EstimationForm/>
-        </div> :<div className="flex  flex-col  w-full">
+        </div> :<div className="flex h-full  flex-col  w-full">
             <ContactOnDialog/>
         </div> }
 
