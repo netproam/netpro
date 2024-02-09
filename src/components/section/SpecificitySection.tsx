aa428301b4bb0a23c17828592984867b089c44d9
Ouzar Merouane <ouzar.merouane@gmail.com>
2024-02-09 01:01:21 -0500

    inspiration de nikita netoyage pour la page temoignage

--- src/components/section/SpecificitySection.tsx
+++ src/components/section/SpecificitySection.tsx
@@ -4,23 +4,23 @@ const SpecificitySection = () => {
    <section className="w-full flex p-4  flex-col items-center gap-4  ">
 
     <div className="flex flex-col w-full px-2 lg:px-8 gap-8 ">
-        <div className="flex flex-col   items-center ">
-            <p className="text-sm font-semibold  pl-1  text-slate-400 
+        <div className="flex flex-col   lg:items-center ">
+            <p className="text-sm font-hind font-light   pl-1  text-slate-400 
             ">{"Une approche innovante".toLocaleUpperCase()}</p>
             <h3 className="tracking-tight 
             lg:pb-4
-            text-2xl lg:text-5xl font-semibold bg-clip-text text-transparent bg-gradient-to-r from-[#04A9F4] to-[#17CCCA]">{`Ce qui fait notre différence`}</h3>
-            <p className="tracking-wide  text-center
+            text-2xl lg:text-5xl font-semibold bg-clip-text text-transparent
+             bg-gradient-to-r from-[#04A9F4] to-[#17CCCA]">{`Ce qui fait notre différence`}</h3>
+            <p className="tracking-wide  lg:text-center
             text-lg font-hind font-light pt-2">{`
-            La solution la plus adaptée :
-            l’entretien des locaux sur mesure.`}</p>
+           pour nous différencier nous nous basons sur plusieurs éléments clés`}</p>
         </div>
-        <div className="grid grid-cols-1 lg:grid-cols-3 text-sm  gap-10 lg:place-items-center ">
+        <div className="grid grid-cols-1 lg:grid-cols-3 text-base font-hind font-light  gap-10 lg:place-items-center ">
 
         <div className="flex flex-col gap-2  lg:items-center lg:text-center">
                 <h4 className="font-semibold text-lg lg:text-3xl font-hind">Qualité
 </h4>
-                <p className="lg:w-2/3 text-sm" >{`Formées, réactives et engagées,
+                <p className="lg:w-2/3 text-base font-hind font-light" >{`Formées, réactives et engagées,
 nos équipes sauront satisfaire vos exigence`}</p>
             </div>
        
@@ -29,31 +29,31 @@ nos équipes sauront satisfaire vos exigence`}</p>
             <div className="flex flex-col gap-2  lg:items-center lg:text-center">
                 <h4 className="font-semibold text-lg lg:text-3xl font-hind">Qualité
 </h4>
-                <p className="lg:w-2/3 text-sm" >{`Formées, réactives et engagées,
+                <p className="lg:w-2/3 text-base font-hind font-light" >{`Formées, réactives et engagées,
 nos équipes sauront satisfaire vos exigence`}</p>
             </div>
             <div className="flex flex-col gap-2  lg:items-center lg:text-center">
                 <h4 className="font-semibold text-lg lg:text-3xl font-hind">Qualité
 </h4>
-                <p className="lg:w-2/3 text-sm" >{`Formées, réactives et engagées,
+                <p className="lg:w-2/3 text-base font-hind font-light" >{`Formées, réactives et engagées,
 nos équipes sauront satisfaire vos exigence`}</p>
             </div>
             <div className="flex flex-col gap-2  lg:items-center lg:text-center" >
                 <h4 className="font-semibold text-lg lg:text-3xl font-hind">Qualité
 </h4>
-                <p className="lg:w-2/3 text-sm" >{`Formées, réactives et engagées,
+                <p className="lg:w-2/3 text-base font-hind font-light" >{`Formées, réactives et engagées,
 nos équipes sauront satisfaire vos exigence`}</p>
             </div>
             <div className="flex flex-col gap-2  lg:items-center lg:text-center">
                 <h4 className="font-semibold text-lg lg:text-3xl font-hind">Qualité
 </h4>
-                <p className="lg:w-2/3 text-sm" >{`Formées, réactives et engagées,
+                <p className="lg:w-2/3 text-base font-hind font-light" >{`Formées, réactives et engagées,
 nos équipes sauront satisfaire vos exigence`}</p>
             </div>
             <div className="flex flex-col gap-2  lg:items-center lg:text-center">
                 <h4 className="font-semibold text-lg lg:text-3xl font-hind" >Qualité
 </h4>
-                <p className="lg:w-2/3 text-sm" >{`Formées, réactives et engagées,
+                <p className="lg:w-2/3 text-base font-hind font-light" >{`Formées, réactives et engagées,
 nos équipes sauront satisfaire vos exigence`}</p>
             </div>
        
