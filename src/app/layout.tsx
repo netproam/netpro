d46afdb152802be3f659959fe9afc28d57808fc7
Ouzar Merouane <ouzar.merouane@gmail.com>
2024-05-26 16:02:55 -0400

    fixation dess bugs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import FooterSection from "@/components/section/FooterSection";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Analytics } from '@vercel/analytics/react';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -24,6 +25,8 @@ export default function RootLayout({
  
         {children}
       <FooterSection/>
+      <Analytics />
+
       </body>
     
     </html>
