931ef2e5719f7a6a18b83e092b9aaf3f4811bde1
Ouzar Merouane <ouzar.merouane@gmail.com>
2024-05-26 15:50:54 -0400

    ajout de l'envoi de l'email

--- src/emails/devisContact.tsx
+++ src/emails/devisContact.tsx
@@ -15,12 +15,12 @@ export default function DevisContact({companyName,subject,emailCompany,phoneClie
       <Text className="font-bold text-xl" >Bonjour, vous avez reçu une nouvelle demande ! </Text>
    <Section className="flex flex-row gap-2  " >
    <Text className="text-lg" >Nom du client : <strong>{companyName}</strong> </Text>
-   <Text className="text-lg" >Le numéro du client : <strong>{phoneClient}</strong> </Text>
+   <Text className="text-lg" >{`Le numéro du client `}: <strong>{phoneClient}</strong> </Text>
 
-      <Text className="text-lg" >L'adresse email du client : <strong>{emailCompany}</strong> </Text>
+      <Text className="text-lg" >{`L'adresse email du client `}: <strong>{emailCompany}</strong> </Text>
 
-      <Text className="text-lg" >L'objet inscrit par le client : <strong >{subject}</strong> </Text>
-      <Text className="text-lg" >Le message du client :  </Text>
+      <Text className="text-lg" >{`L'objet inscrit par le client`} : <strong >{subject}</strong> </Text>
+      <Text className="text-lg" >{`Le message du client : `} </Text>
       <Hr className="w-full"/>
       <Text className="text-xl pt-2" > {messaggeClient} </Text>
 
